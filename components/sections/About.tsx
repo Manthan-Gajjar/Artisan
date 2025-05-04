8cbac303e0c46e4cc3feaf5e9a04b8b26803ee0d
Manthan-Gajjar <mgdev.coder@gmail.com>
2025-05-04 12:35:00 +0530

    latest code

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Image from 'next/image';
+import Image from 'next/image'; 
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
